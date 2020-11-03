e8aa9283d388bda3ca46c2762d5775e23b1d073a
Robbie Meyer <robbie.meyer12@gmail.com>
2020-11-03 17:57:48 -0500

    Log links found in tweets

--- hoth.js
+++ hoth.js
@@ -1,2 +1,23 @@
-var links = $("article a");
-console.log(links);
+const observerConfig = { childList: true, subtree: true };
+
+const pageChangeCallback = function(mutationsList, observer) {
+  mutationsList.map(function(record) {
+    if (record.addedNodes.length > 0)
+      return $(record.addedNodes[0]).find("article");
+  }).filter(function(article) {
+    return article && article.length > 0;
+  }).forEach(logLink);
+}
+
+const logLink = function(article) {
+  var links = article.find('a[href*="t.co"');
+  console.log(links);
+};
+
+
+// TODO: Make not hacky
+setTimeout(function() {
+  const primaryColumn = $("main div").get(0)
+  const observer = new MutationObserver(pageChangeCallback);
+  observer.observe(primaryColumn, observerConfig);
+}, 500);
