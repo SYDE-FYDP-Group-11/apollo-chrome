c37e06175c313799871f8005e8bcc36f4b0f1643
Robbie Meyer <robbie.meyer12@gmail.com>
2020-11-06 15:24:53 -0500

    Update annotator to add seperate indicators

--- annotator.js
+++ annotator.js
@@ -1,10 +1,29 @@
-const createAnnotator = function (article) {
+const createAnnotator = function(article) {
   return function (annotations) {
+    // Temporary 
+    const tags = {'one': true, 'two': false, 'three': false}
+
     let actionBox = article.querySelector('[role="group"]');
     let hothBox = document.createElement("div");
     hothBox.className = "hoth";
-    let content = document.createTextNode(JSON.stringify(annotations));
-    hothBox.appendChild(content);
+
+    let hothBoxInner = document.createElement("div");
+    hothBox.appendChild(hothBoxInner);
+
+    Object.entries(tags)
+      .map(toIndicator)
+      .forEach(hothBoxInner.appendChild.bind(hothBoxInner));
+
     actionBox.insertAdjacentElement("beforebegin", hothBox);
   }
 };
+
+const toIndicator = function([key, value]) {
+  let span = document.createElement("span");
+  let content = document.createTextNode(key);
+  span.className = "hoth-indicator hoth-" + key;
+  if (typeof value == "boolean")
+    span.className += " hoth-" + value.toString(); 
+  span.appendChild(content);
+  return span
+}
