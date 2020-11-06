f83c9785d5ea210841966b0801256d92875c42de
Ryan Mok <ryan-mok@users.noreply.github.com>
2020-11-06 15:37:01 -0500

    Remove unnecessary anonymous function

--- background.js
+++ background.js
@@ -3,7 +3,7 @@ chrome.runtime.onMessage.addListener(
 		if (request.contentScriptQuery == "queryApi") {
 			fetch(`https://hoth-server.herokuapp.com/api?url=${request.url}`)
 				.then(response => response.json())
-				.then(json => sendResponse(json))
+				.then(sendResponse)
 				.catch(err => console.log("Error: " + err.message)
 			);
 			return true;
