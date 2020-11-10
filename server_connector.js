8bbdfee6bb7de3a9e25bcc26acab0214e1908deb
Ryan Mok <ryan-mok@users.noreply.github.com>
2020-11-10 14:32:33 -0500

    Remove debugging code

--- server_connector.js
+++ server_connector.js
@@ -1,8 +1,4 @@
 const getAnnotations = function (url, callback) {
-  // For debugging purposes
-  random = Math.floor((Math.random() * 3) + 1);
-  url = `http://test.com/${random}`;
-  
   chrome.runtime.sendMessage(
     {contentScriptQuery: "queryApi", url: url}, callback
   );
