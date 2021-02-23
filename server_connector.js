68ff95f53dff0b0889dd1bd2c040783760e1d571
Ryan Mok <ryan-mok@users.noreply.github.com>
2021-02-22 23:58:00 -0500

    Add inline comments to explain process

--- server_connector.js
+++ server_connector.js
@@ -10,10 +10,18 @@ port.onMessage.addListener(msg => {
   if (msg.tweet_id == current_tweet_id) {
     switch (msg.type) {
       case 'related_articles':
+        // Call function here to update related articles with content in msg.content
+        // msg.tweet_id is filtered to last clicked Tweet
+        // msg.event is used to filter for related articles results
+        // msg.content contains the result from NewsAPI
         console.log('Related Articles')
         console.log(msg.content)
         break;
       case 'sentiment_analysis':
+        // Call function here to update sentiment anaylsis with content in msg.content
+        // msg.tweet_id is filtered to last clicked Tweet
+        // msg.event is used to filter for sentiment analysis results
+        // msg.content contains the result from sentiment analysis API
         console.log('Sentiment Analysis')
         console.log(msg.content)
         break;
