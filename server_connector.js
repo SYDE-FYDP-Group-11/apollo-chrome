a696bd6e9f502da62860e5716d55f17542b87056
Ryan Mok <ryan-mok@users.noreply.github.com>
2021-02-27 13:36:41 -0500

    Show article info structured in sidebar

--- server_connector.js
+++ server_connector.js
@@ -1,30 +1,27 @@
-const port = chrome.runtime.connect({ name: 'apollo' })
-let current_tweet_id = ''
+class ServerConnector {
+  constructor(sidebar) {
+    this.port = chrome.runtime.connect({ name: 'apollo' })
+    this.current_tweet_id = ''
 
-const requestTweetData = function (tweet_id) {
-  current_tweet_id = tweet_id
-  port.postMessage({ tweet_id: tweet_id })
-}
-
-port.onMessage.addListener(msg => {
-  if (msg.tweet_id == current_tweet_id) {
-    switch (msg.type) {
-      case 'related_articles':
-        // Call function here to update related articles with content in msg.content
-        // msg.tweet_id is filtered to last clicked Tweet
-        // msg.event is used to filter for related articles results
-        // msg.content contains the result from NewsAPI
-        console.log('Related Articles')
-        console.log(msg.content)
-        break;
-      case 'sentiment_analysis':
-        // Call function here to update sentiment anaylsis with content in msg.content
-        // msg.tweet_id is filtered to last clicked Tweet
-        // msg.event is used to filter for sentiment analysis results
-        // msg.content contains the result from sentiment analysis API
-        console.log('Sentiment Analysis')
-        console.log(msg.content)
-        break;
-    }
+    this.port.onMessage.addListener(msg => {
+      if (msg.tweet_id == this.current_tweet_id) {
+        switch (msg.type) {
+          case 'article_info':
+            sidebar.addArticleInfo(msg.content)
+            break
+          case 'sentiment_analysis':
+            sidebar.addSentimentAnalysis(msg.content)
+            break
+          case 'related_articles':
+            sidebar.addRelatedArticles(msg.content)
+            break
+        }
+      }
+    })
   }
-})
+
+  requestTweetData(tweet_id) {
+    this.current_tweet_id = tweet_id
+    this.port.postMessage({ tweet_id: tweet_id })
+  }
+}
--- sidebar.js
+++ sidebar.js
@@ -1,31 +1,54 @@
 class Sidebar {
   constructor() {
-    this.sidebar = document.createElement("div");
-    this.sidebar.className = "apollo-sidebar apollo apollo-hidden";
-
-    let header = document.createElement("div");
-    header.className = "apollo-header";
-    header.appendChild(document.createTextNode("Apollo"));
-    this.sidebar.appendChild(header);
-
-    let close_button = document.createElement("span");
-    close_button.className = "apollo-close";
-    close_button.appendChild(document.createTextNode("\u2715"));
-    close_button.onclick = this.close.bind(this);
-    header.appendChild(close_button);
-
-    let box = document.createElement("div");
-    box.className = "apollo-box";
-    this.sidebar.appendChild(box);
-
-    this.textbox = document.createElement("pre");
-    this.textbox.className = "apollo-text";
-    box.appendChild(this.textbox);
-
+    this.sidebar = document.createElement("div")
+    this.sidebar.className = "apollo-sidebar apollo apollo-hidden"
+    this.sidebar.innerHTML = `
+      <div class="apollo-header">
+        Apollo
+        <span class="apollo-close" id="apollo-close">\u2715</span>
+      </div>
+      <div class="apollo-box">
+        <div>
+          <img id="apollo-image"></img> 
+          <div>Headline</div>
+          <div id="apollo-headline"></div>
+          <div>Excerpt</div>
+          <div id="apollo-excerpt"></div>
+          <div>Site</div>
+          <div id="apollo-site"></div>
+          <div>Last Updated <span id="apollo-date"></span></div>
+        </div>
+        <div>
+          <div>Author</div>
+          <div id="apollo-author"></div>
+        </div>
+      </div>
+    `
     document.body.appendChild(this.sidebar);
+
+    document.getElementById('apollo-close').onclick = this.close.bind(this)
+
+    this.image = document.getElementById('apollo-image')
+    this.headline = document.getElementById('apollo-headline')
+    this.excerpt = document.getElementById('apollo-excerpt')
+    this.site = document.getElementById('apollo-site')
+    this.date = document.getElementById('apollo-date')
+    this.author = document.getElementById('apollo-author')
+  }
+
+  clear() {
+    this.image.src = null
+    if (!this.image.classList.contains("apollo-hidden"))
+      this.image.classList.add("apollo-hidden")
+    this.headline.innerHTML = null
+    this.excerpt.innerHTML = null
+    this.site.innerHTML = null
+    this.date.innerHTML = null
+    this.author.innerHTML = null
   }
 
   open() {
+    this.clear()
     if (this.sidebar.classList.contains("apollo-hidden"))
       this.sidebar.classList.remove("apollo-hidden");
   }
@@ -34,8 +57,24 @@ class Sidebar {
     this.sidebar.classList.add("apollo-hidden");
   }
 
-  add_text(text) {
-    this.textbox.innerHTML = JSON.stringify(text, null, 2);
+  addArticleInfo(json) {
+    if (json.image) {
+      this.image.src = json.image
+      this.image.classList.remove("apollo-hidden")
+    }
+    this.headline.innerHTML = json.title
+    this.excerpt.innerHTML = json.excerpt
+    this.site.innerHTML = json.site
+    this.date.innerHTML = json.date
+    this.author.innerHTML = json.byline
+  }
+
+  addSentimentAnalysis(json) {
+    // TODO
+  }
+
+  addRelatedArticles(json) {
+    // this.textbox.innerHTML = JSON.stringify(json, null, 2);
   }
 }
 
