0889c49b4abd75c2aeb2fbc0a6618a842ba4a86c
Robbie Meyer <robbie.meyer12@gmail.com>
2021-03-12 13:50:34 -0500

    Add comments to apollo.js

--- apollo.js
+++ apollo.js
@@ -1,10 +1,14 @@
-const observerConfig = { childList: true, subtree: true };
+// This file contains code that monitors the Twitter page for new Tweets
+// When a Tweet is found, the Apollo button will be inserted and set-up
 
 const toPrimaryColumn = function (record) {
   if (record.addedNodes.length > 0)
     return record.addedNodes[0].querySelector('main div')
 };
 
+// Page level mutation observer callback
+// Looks for the primary column to be found (so we don't need
+// to monitor the entire page)
 const observerCallback = function(mutationsList, observer) {
   if (!this.foundPrimaryColumn) {
     const primaryColumn = mutationsList.map(toPrimaryColumn).filter(x => x);
@@ -24,6 +28,8 @@ const toArticle = function (record) {
     return record.addedNodes[0].querySelector("article");
 }
 
+// Process a change in the primary column
+// Looks for new tweets and adds them to the tweetObserver
 const processChange = function(mutationsList) {
   mutationsList.map(toArticle).filter(x => x)
     .forEach(function(article) {
@@ -38,10 +44,12 @@ const tweetObserverCallback = function (mutationsList, observer) {
     processArticle(article);
 }
 
+// Process a loaded <article> tag (a loaded tweet)
 const processArticle = function(article) {
   if (article.dataset.apolloed)
     return;
 
+  // Only add button to Tweets containing external links
   const article_link = article.querySelector('a[href^="https://t.co/"]');
   if (!article_link)
     return;
@@ -54,6 +62,8 @@ const processArticle = function(article) {
 
   const card = article_link.closest('[data-testid="card.wrapper"]');
   const button = addButton(article, article_link, card);
+
+  // When the button is clocked, make a request using the TweetID
   button.onclick = () => {
     sidebar.open();
     addPersistentHighlight(card || article_link);
@@ -65,11 +75,15 @@ const processArticle = function(article) {
 };
 
 const sidebar = new Sidebar();
-
 const serverConnector = new ServerConnector(sidebar)
 
+const observerConfig = { childList: true, subtree: true };
+
+// Create mutation observer that will eventually be used to watch
+// Tweets
 const tweetObserver = new MutationObserver(tweetObserverCallback);
 
+// Create mutation observer to watch for new Tweets
 const root = document.getElementById("react-root");
 const observer = new MutationObserver(observerCallback);
 observer.observe(root, observerConfig);
